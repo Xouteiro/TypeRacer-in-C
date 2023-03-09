4313f0b3bf2b39c48ee6ef3b5b28a2c2e39be8cb
coutinho21 <guilhermecscoutinho@gmail.com>
2023-03-09 23:58:05 +0000

    Added lab2 (not finished)

---
+++ lab2/lab2.c
@@ -0,0 +1,56 @@
+#include <lcom/lcf.h>
+#include <lcom/lab2.h>
+
+#include <stdbool.h>
+#include <stdint.h>
+
+int main(int argc, char *argv[]) {
+  // sets the language of LCF messages (can be either EN-US or PT-PT)
+  lcf_set_language("EN-US");
+
+  // enables to log function invocations that are being "wrapped" by LCF
+  // [comment this out if you don't want/need it]
+  lcf_trace_calls("/home/lcom/labs/lab2/trace.txt");
+
+  // enables to save the output of printf function calls on a file
+  // [comment this out if you don't want/need it]
+  lcf_log_output("/home/lcom/labs/lab2/output.txt");
+
+  // handles control over to LCF
+  // [LCF handles command line arguments and invokes the right function]
+  if (lcf_start(argc, argv))
+    return 1;
+
+  // LCF clean up tasks
+  // [must be the last statement before return]
+  lcf_cleanup();
+
+  return 0;
+}
+
+int(timer_test_read_config)(uint8_t timer, enum timer_status_field field) {
+  /* To be implemented by the students */
+  printf("%s is not yet implemented!\n", __func__);
+
+  return 1;
+}
+
+int(timer_test_time_base)(uint8_t timer, uint32_t freq) {
+  /* To be implemented by the students */
+  printf("%s is not yet implemented!\n", __func__);
+
+  return 1;
+}
+
+int(timer_test_int)(uint8_t time) {
+  uint8_t timer_mask;
+  if(timer_sunscribe_int)
+  uint16_t n_interrupt = time*60;
+  int i = 0;
+  while( i <= n_interrupt){
+    //copiar souto
+    if(IRQ_line & timer_mask) i++;
+  }
+  if(timer_unsubcribe_int()) return 1;
+  return 0;
+}
